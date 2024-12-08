c7fd34a27a552b84bd76485675fc5b8df9881fcc
tae <x1337@taes-Laptop.local>
2024-12-08 16:08:22 +0100

    added modules dir and getstarted

---
+++ getstarted.c
@@ -0,0 +1,8 @@
+#include <kiwi.h>
+#include <stdio.h>
+
+int main() {
+    kw_init();
+    
+    return 0;
+}
--- kiwi.c
+++
@@ -1,7 +0,0 @@
-#include <kiwi.h>
-#include <stdio.h>
-
-int main() {
-    kw_init();
-    return 0;
-}
