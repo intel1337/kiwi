e8c1da15bfba67de9f2f1733e64a97899367c582
tae <x1337@Mac.mshome.net>
2024-12-09 04:43:44 +0100

    Changed .h and started cli

--- getstarted.c
+++ getstarted.c
@@ -4,10 +4,10 @@
 int main() {
     kw_init();
     char name[100];
-    int test;
+    int* test;
     input("Enter your name: ", name, 100);
     printf("Hello, %s!\n", name);
-    iinput("Enter a number: ", test, 100);
+    iinput("Enter a number: ", &test, 100);
     printf("You entered: %d\n", test);
     return 0;
 }
--- kwframework/modules/iinput.c
+++ kwframework/modules/iinput.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 
-int iinput(char prompt[], int var, int size) {
+int iinput(char prompt[], int* var, int size) {
     printf("%s", prompt);
     char input[size];
     fgets(input, size, stdin);
-    var = atoi(input);
+    *var = atoi(input);
 
 }
 
